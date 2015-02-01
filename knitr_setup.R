8344a16eb5d35aafc076ff8327865ddb3cf8f7bc
droglenc <dogle@northland.edu>
2015-02-01 16:48:54 -0600

    Slight modification to knitr_setup

--- knitr_setup.R
+++ knitr_setup.R
@@ -15,9 +15,11 @@ knit_hooks$set(par2 = function(before, options, envir) {
 opts_chunk$set(prompt=TRUE,comment='')
 ## knitr options -- figure handling
 opts_chunk$set(fig.path='Figs/', fig.width=3.5, fig.height=3.5, par1=TRUE)
+
 ## removes spaces between input and output chunks
-#hook1 <- function(x){ gsub("```\n+```\n", "", x) }
-#knit_hooks$set(document = hook1)
+## this works ok with the haddock theme, not with others
+hook1 <- function(x){ gsub("```\n+```\n", "", x) }
+knit_hooks$set(document = hook1)
 
 ## r options
 options(width=100,show.signif.stars=FALSE,continue=" ")
